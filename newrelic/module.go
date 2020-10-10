f1598ec52d1bc51f241813d8d10c2abe60da475a
Lucas Katayama <lucaskatayama@gmail.com>
2020-10-10 13:16:02 -0300

    newrelic

---
+++ newrelic/module.go
@@ -0,0 +1,20 @@
+package newrelic
+
+import (
+	"os"
+
+	"github.com/newrelic/go-agent/v3/newrelic"
+	"go.uber.org/fx"
+)
+
+var Module = fx.Options(
+	fx.Provide(new),
+)
+
+func new() (*newrelic.Application, error) {
+	return newrelic.NewApplication(
+		newrelic.ConfigAppName(os.Getenv("NEWRELIC_APPNAME")),
+		newrelic.ConfigLicense(os.Getenv("NEWRELIC_LICENSEKEY")),
+	)
+
+}
