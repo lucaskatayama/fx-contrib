96a5d677b628a7e1b2fed5c70e8838d906c41e3c
Lucas Katayama <lucaskatayama@gmail.com>
2020-10-11 13:41:02 -0300

    docs

--- httpserver/doc_test.go
+++ httpserver/doc_test.go
@@ -13,3 +13,13 @@ func Example() {
 	)
 	app.Run()
 }
+
+// Setting HOST and PORT
+func ExampleModule_example1() {
+	// export HOST=0.0.0.0
+	// export PORT=8080
+	app := fx.New(
+		httpserver.Module,
+	)
+	app.Run()
+}
